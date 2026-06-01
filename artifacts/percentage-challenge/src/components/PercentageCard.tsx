015058966a2efe05a3cee210fa1c93ff1f18d1a6
MohamedRadiWebDev <141498530+MohamedRadiWebDev@users.noreply.github.com>
2026-06-01 16:32:54 +0300

    Update text in PercentageCard component

--- artifacts/percentage-challenge/src/components/PercentageCard.tsx
+++ artifacts/percentage-challenge/src/components/PercentageCard.tsx
@@ -74,8 +74,11 @@ export default function PercentageCard({ percentage, onPercentageChange }: Perce
         <h1
           className="text-xl font-black tracking-tight"
           style={{ color: "#be185d", direction: "rtl" }}
-        >مؤشر الصلح
-        علي السكة تقييمك ل محمد بعد اخر خناقة </h1>
+        >
+    
+          مؤشر الصلح
+        <br></br>
+          علي السكة تقييمك ل محمد بعد اخر خناقة </h1>
         <p className="text-pink-400 mt-0.5 text-[16px] font-bold" style={{ direction: "rtl" }}>
           حركي السلايدر وقولي الصراحة متخافيش🔪
         </p>
